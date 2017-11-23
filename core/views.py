3e24ff9ce9bc1b0182a99f332652d7932f44d306
vit.dev <vit.silva.dev@gmail.com>
2017-11-23 09:03:19 -0200

    My first git

---
+++ core/views.py
@@ -0,0 +1,13 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+
+def index(request):
+    return render(request, 'index.html')
+
+def produtos(request):
+    return render(request, 'produtos.html')
+
+def contato(request):
+    return render(request, 'contato.html')
